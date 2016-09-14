4d8c339926925958facb21062f452cb07213095d
Alexander Wong <admin@alexander-wong.com>
2016-09-14 11:22:46 -0600

    SVG test

---
+++ udia/App/styles/colors.js
@@ -0,0 +1,6 @@
+
+export const RICH_BLACK = '#07090F';   // Background
+export const ONYX = '#3F3244';         // Selected Links
+export const GAINSBORO = '#D9D9D9';    // Text
+export const OLD_LAVENDER = '#77567A'; // Unselected Links
+export const GUNMETAL = '#2F323A';     // Sub Background
